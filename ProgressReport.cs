d029599f328562e9f87fb38a47140b2565c53b79
Debra L Quarles <121737563+Debq2001@users.noreply.github.com>
2024-07-06 17:35:08 -0700

    Add files via upload

---
+++ ProgressReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR_CMD_EXT_TO_FILE
+{
+     class ProgressReport
+    {
+        public int PercentComplete { get; set; }    
+
+    }
+}
